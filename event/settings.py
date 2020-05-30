d4bfbd5b4dc5d65e7a6fb8099ecc8e65b0883e45
Jugal-Chanda <jugalchanda7@gmail.com>
2020-05-31 01:23:23 +0600

    Upload Installed pakage name

--- event/settings.py
+++ event/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
     'accounts',
     'club',
     'mainadmin',
